418ee8b34b2cd59fb5f123ce51d36d87b938f159
gharkness <garrett1440@gmail.com>
2016-09-29 13:50:27 -0500

    graph and main files

--- graph.js
+++ graph.js
@@ -117,5 +117,6 @@ g.addVertex('F', {B: 2, C: 6, D: 8, G: 9, H: 3});
 g.addVertex('G', {C: 4, F: 9});
 g.addVertex('H', {E: 1, F: 3});
 
+
 // Log test, with the addition of reversing the path and prepending the first node so it's more readable
 console.log(g.dijkstra('A', 'H').concat(['A']).reverse());
